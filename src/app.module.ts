009a3ed54355a8d68d370b75830f7c499e51fc25
Adewoye Adegoke <69061153+Degoke@users.noreply.github.com>
2022-01-13 19:29:31 +0100

    setup dev enviroment
    setup docker, postgres, pgAdmin installed dependencies

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,33 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [],
   controllers: [AppController],
   providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        PORT: Joi.number(),
+        POSTGRES_USER: Joi.string().required(),
+        POSTGRES_PASSWORD: Joi.string().required(),
+        POSTGRES_DB_PREFIX: Joi.string().required(),
+        POSTGRES_DB_NAME: Joi.string().required(),
+        POSTGRES_DB_PORT: Joi.number().required(),
+        POSTGRES_DB_HOST: Joi.string().required(),
+        PGADMIN_DEFAULT_EMAIL: Joi.string().required(),
+        PGADMIN_DEFAULT_PASSWORD: Joi.string().required(),
+      }),
+    }),
+    AuthModule,
+    UserModule,
+    DatabaseModule,
+  ],
 })
 export class AppModule {}
