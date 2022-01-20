f593802e54186f94dabbcea13dc7d1bed68de02a
Adewoye Adegoke <69061153+Degoke@users.noreply.github.com>
2022-01-20 10:11:01 +0100

    take it

--- src/flutterwave/dto/initiate-withdrawal.dto.ts
+++ src/flutterwave/dto/initiate-withdrawal.dto.ts
@@ -1,27 +1,27 @@
-import { IsString, IsNotEmpty } from "class-validator"
+import { IsString, IsNotEmpty } from 'class-validator';
 
 export class InitiateWithdrawalDto {
-    @IsString()
-    @IsNotEmpty()
-    account_bank
+  @IsString()
+  @IsNotEmpty()
+  account_bank;
 
-    @IsString()
-    @IsNotEmpty()
-    account_number
+  @IsString()
+  @IsNotEmpty()
+  account_number;
 
-    @IsString()
-    @IsNotEmpty()
-    amount
+  @IsString()
+  @IsNotEmpty()
+  amount;
 
-    @IsString()
-    @IsNotEmpty()
-    narration
+  @IsString()
+  @IsNotEmpty()
+  narration;
 
-    @IsString()
-    @IsNotEmpty()
-    currency
+  @IsString()
+  @IsNotEmpty()
+  currency;
 
-    @IsString()
-    @IsNotEmpty()
-    beneficiary_name
+  @IsString()
+  @IsNotEmpty()
+  beneficiary_name;
 }
