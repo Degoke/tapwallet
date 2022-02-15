8406bb6aad4f3743b2b035cd7aa4972c77696a54
Adewoye Adegoke <69061153+Degoke@users.noreply.github.com>
2022-02-15 16:50:44 +0100

    redis password

--- src/app.module.ts
+++ src/app.module.ts
@@ -61,6 +61,7 @@ import { NodemailerModule } from './nodemailer/nodemailer.module';
       store: redisStore,
       host: process.env.REDIS_HOST,
       port: process.env.REDIS_PORT,
+      password: process.env.REDIS_PASSWORD,
       ttl: 1200,
       isGlobal: true,
     }),
